9b1aa34f79deacfde546ca0031c1449a89690b13
Danilo <danilo.silva@msn.com>
2022-10-02 01:40:57 -0300

    Criação dos Projetos e Definição das Referências

---
+++ DevStore/DevStore.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevStore.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
